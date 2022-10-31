3ff5720891d59e993d4c938ec2cd70d976a4ad43
sebastienmariette74 <61980470+sebastienmariette74@users.noreply.github.com>
2022-10-31 14:26:18 +0100

    delete file MissionTypeCopy

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Nat\DeployBundle\NatDeployBundle::class => ['all' => true],
 ];
